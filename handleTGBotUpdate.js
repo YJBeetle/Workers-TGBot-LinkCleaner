cd12cbb36bb9996400a2ee4b17468de8036a91d3
YJBeetle <yjbeetle@gmail.com>
2023-03-23 19:51:28 +0800

    新的 handleUrl函数剥离用于处理url   在消息处理部分 支持多个链接   更换url匹配的正则

--- handleTGBotUpdate.js
+++ handleTGBotUpdate.js
@@ -1,5 +1,21 @@
 import { requestTelegramBotAPI } from "./telegram";
 
+async function handleUrl(originalLink) {
+    const TWIpattern = /https:\/\/(vx)?twitter\.com/g;
+
+    let cleanLink = originalLink.replace(/\?.*$/g, "");
+
+    if (TWIpattern.test(originalLink)) {
+        cleanLink = cleanLink.replace(TWIpattern, "https://vxtwitter.com");
+    } else {
+        const result = await fetch(originalLink, { method: "HEAD", redirect: "manual" });
+        const location = result.headers.get("location") ?? originalLink;
+        cleanLink = location.replace(/\?.*$/g, "");
+    }
+
+    return cleanLink;
+}
+
 async function handleCommand({ text, chat }) {
     const commandEndPos = text.indexOf(' ');
     const command = text.substring(1, commandEndPos == -1 ? undefined : commandEndPos).toLowerCase();
@@ -20,36 +36,22 @@ async function handleCommand({ text, chat }) {
 }
 
 async function handleText({ text, chat, message_id }) {
-    const URLpattern = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-./?%&=+#]*)?/g;
-    const TWIpattern = /https:\/\/(vx)?twitter\.com/g;
-
-    const rawLinks = text.match(URLpattern);
+    const rawLinks = text.match(/(https?):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/gs);
     if (rawLinks) {
-        const [originalLink] = rawLinks;
-        let cleanLink = originalLink.replace(/\?.*$/g, "");
+        let replytext = ''
 
-        if (TWIpattern.test(originalLink)) {
-            cleanLink = cleanLink.replace(TWIpattern, "https://vxtwitter.com");
-        } else {
-            const result = await fetch(originalLink, { method: "HEAD", redirect: "manual" });
-            const location = result.headers.get("location") ?? originalLink;
-            cleanLink = location.replace(/\?.*$/g, "");
-        }
+        for (const url of rawLinks) {
+            const cleanedUrl = await handleUrl(url)
+            if (cleanedUrl !== url)
+                replytext += cleanedUrl + '\n'
+        };
+        if (!replytext && chat.type !== "private") replytext = "该链接不需要清理跟踪参数哦，如果你认为这是个错误请向开发者反馈~";
 
-        const replyMessage = { chat_id: chat.id };
-
-        if (originalLink !== cleanLink) {
-            replyMessage.text = cleanLink;
-        } else {
-            replyMessage.text = "该链接不需要清理跟踪参数哦，如果你认为这是个错误请向开发者反馈~"
-        }
-
-        // 如果不是私聊则引用回复
-        if (message.chat.type !== "private") {
-            replyMessage.reply_to_message_id = message_id;
-        }
-
-        await requestTelegramBotAPI("sendMessage", replyMessage);
+        await requestTelegramBotAPI("sendMessage", {
+            chat_id: chat.id,
+            text: replytext,
+            reply_to_message_id: (chat.type !== "private") ? message_id : null
+        });
     }
 }
 
